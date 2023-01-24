9005e37b8d9eab3b7a4938b11a60d260d63896fd
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-01-24 13:49:08 +0000

    Added text to markdown template

---
+++ README.md
@@ -0,0 +1,30 @@
+# Title
+
+  ## Description
+  ## Table of Contents
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+  ## Installation
+  ## Usage
+  ## License
+  ## Contributing
+  ## Tests
+  ## Questions
+  
+  My GitHub username is Nwinch1512.
+  
+  [GitHub page](https://github.com/Nwinch1512/ "Visit my GitHub page")
+ 
+  My email address is undefined.
+
+  [Contact Me](mailto:undefined)
+  
+  ## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 // template for creating README file
+// const fs = require("fs");
+// const path = require("path");
+// const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+// // array of questions for user
+// const questions = [];
+
+// // function to write README file
+// function writeToFile(fileName, data) {}
+
+// // function to initialize program
+// function init() {}
+
+// // function call to initialize program
+// init();
+
+// My first attempt at creating README generator
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
