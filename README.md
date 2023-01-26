093f57aca0e4afb3ac39d98a5f411c5d4416e0a2
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-01-26 11:58:34 +0000

    Added GitHub licenses as object of object ready to populate badge on README

---
+++ README.md
@@ -0,0 +1,48 @@
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+![License](https://img.shields.io/badge/License-BSD_2--Clause-blue.svg)
+
+# Title
+
+## Table of Contents
+
+[Description](#description)<br>
+[Installation](#installation)<br>
+[Usage](#usage)<br>
+[License](#license)<br>
+[Contributing](#contributing)<br>
+[Tests](#tests)<br>
+[Questions](#questions)
+
+## Description
+
+This project helps to generate a README for projects.
+
+## Installation
+
+These steps should be followed to install this project: Open the index.js file in the integrated terminal window. Type "node index.js" in node js. Provide answers to all of the prompt questions..
+
+## Usage
+
+This project should be used to to generate a README file..
+
+## License
+
+This project holds a MIT license.
+
+## Contributing
+
+The following: Node JS. contributed to this project.
+
+## Tests
+
+The project should be test in the following way: N/A.
+
+## Questions
+
+My GitHub username is Nwinch1512.
+
+[GitHub page](https://github.com/Nwinch1512/ "Visit my GitHub page")
+
+My email address is undefined.
+
+[Contact Me](mailto:undefined)
--- index.js
+++ index.js
@@ -17,12 +17,179 @@
 // init();
 
 // My first attempt at creating README generator
-
-const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+const licences = {
+  "Apache 2.0 License": {
+    url: "https://opensource.org/licenses/Apache-2.0",
+    imageUrl: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+  },
+  "Boost Software License 1.0": {
+    url: "https://www.boost.org/LICENSE_1_0.txt",
+    imageUrl: "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
+  },
+  "BSD 3-Clause License": {
+    url: "https://opensource.org/licenses/BSD-3-Clause",
+    imageUrl: "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+  },
+  "BSD 2-Clause License": {
+    url: "https://opensource.org/licenses/BSD-2-Clause",
+    imageUrl: "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg",
+  },
+  CC0: {
+    url: "http://creativecommons.org/publicdomain/zero/1.0/",
+    imageUrl: "https://licensebuttons.net/l/zero/1.0/80x15.png",
+  },
+  "CC0 1.0": {
+    url: "http://creativecommons.org/publicdomain/zero/1.0/",
+    imageUrl: "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
+  },
+  "Attribution 4.0 International": {
+    url: "https://creativecommons.org/licenses/by/4.0/",
+    imageUrl: "https://licensebuttons.net/l/by/4.0/80x15.png",
+  },
+  "CC BY 4.0": {
+    url: "https://creativecommons.org/licenses/by/4.0/",
+    imageUrl: "https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg",
+  },
+  "Attribution-ShareAlike 4.0 International": {
+    url: "https://creativecommons.org/licenses/by-sa/4.0/",
+    imageUrl: "https://licensebuttons.net/l/by-sa/4.0/80x15.png",
+  },
+  "CC BY-SA 4.0": {
+    url: "https://creativecommons.org/licenses/by-sa/4.0/",
+    imageUrl:
+      "https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg",
+  },
+  "CC BY-NC 4.0": {
+    url: "https://creativecommons.org/licenses/by-nc/4.0/",
+    imageUrl:
+      "https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
+  },
+  "Attribution-NoDerivates 4.0 International": {
+    url: "https://creativecommons.org/licenses/by-nc/4.0/",
+    imageUrl: "https://licensebuttons.net/l/by-nc/4.0/80x15.png",
+  },
+  "CC BY-ND 4.0": {
+    url: "https://creativecommons.org/licenses/by-nc/4.0/",
+    imageUrl:
+      "https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
+  },
+  "Attribution-NonCommmercial-ShareAlike 4.0 International": {
+    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+    imageUrl: "https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png",
+  },
+  "CC BY-NC-SA 4.0": {
+    url: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    imageUrl:
+      "https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg",
+  },
+  "Attribution-NonCommercial-NoDerivatives 4.0 International": {
+    url: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    imageUrl: "https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png",
+  },
+  "CC BY-NC-ND 4.0": {
+    url: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    imageUrl:
+      "https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg",
+  },
+  "Eclipse Public License 1.0": {
+    url: "https://opensource.org/licenses/EPL-1.0",
+    imageUrl: "https://img.shields.io/badge/License-EPL_1.0-red.svg",
+  },
+  "GNU GPL v3": {
+    url: "https://www.gnu.org/licenses/gpl-3.0",
+    imageUrl: "https://img.shields.io/badge/License-GPLv3-blue.svg",
+  },
+  "GNU GPL v2": {
+    url: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+    imageUrl: "https://img.shields.io/badge/License-GPL_v2-blue.svg",
+  },
+  "GNU AGPL v3": {
+    url: "https://www.gnu.org/licenses/agpl-3.0",
+    imageUrl: "https://img.shields.io/badge/License-AGPL_v3-blue.svg",
+  },
+  "GNU LGPL v3": {
+    url: "https://www.gnu.org/licenses/lgpl-3.0",
+    imageUrl: "https://img.shields.io/badge/License-LGPL_v3-blue.svg",
+  },
+  "GNU FDL v1.3": {
+    url: "https://www.gnu.org/licenses/fdl-1.3",
+    imageUrl: "https://img.shields.io/badge/License-FDL_v1.3-blue.svg",
+  },
+  "The Hippocratic License 2.1": {
+    url: "https://firstdonoharm.dev",
+    imageUrl:
+      "https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg",
+  },
+  "The Hippocratic License 3.0": {
+    url: "https://firstdonoharm.dev",
+    imageUrl:
+      "https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg",
+  },
+  "IBM Public License Version 1.0": {
+    url: "https://opensource.org/licenses/IPL-1.0",
+    imageUrl: "https://img.shields.io/badge/License-IPL_1.0-blue.svg",
+  },
+  "ISC License (ISC)": {
+    url: "https://opensource.org/licenses/ISC",
+    imageUrl: "https://img.shields.io/badge/License-ISC-blue.svg",
+  },
+  MIT: {
+    url: "https://opensource.org/licenses/MIT",
+    imageUrl: "https://img.shields.io/badge/License-MIT-yellow.svg",
+  },
+  "Mozilla Public License 2.0": {
+    url: "https://opensource.org/licenses/MPL-2.0",
+    imageUrl: "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+  },
+  "Attribution License (BY)": {
+    url: "https://opendatacommons.org/licenses/by/",
+    imageUrl: "https://img.shields.io/badge/License-ODC_BY-brightgreen.svg",
+  },
+  "Open Database License (ODbL)": {
+    url: "https://opendatacommons.org/licenses/odbl/",
+    imageUrl: "https://img.shields.io/badge/License-ODbL-brightgreen.svg",
+  },
+  "Public Domain Dedication and License (PDDL)": {
+    url: "https://opendatacommons.org/licenses/pddl/",
+    imageUrl: "https://img.shields.io/badge/License-PDDL-brightgreen.svg",
+  },
+  "The Perl License": {
+    url: "https://opensource.org/licenses/Artistic-2.0",
+    imageUrl: "https://img.shields.io/badge/License-Perl-0298c3.svg",
+  },
+  "The Artistic License 2.0": {
+    url: "https://opensource.org/licenses/Artistic-2.0",
+    imageUrl: "https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg",
+  },
+  "SIL Open Font License 1.1": {
+    url: "https://opensource.org/licenses/OFL-1.1",
+    imageUrl: "https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg",
+  },
+  Unlicense: {
+    url: "http://unlicense.org/",
+    imageUrl: "https://img.shields.io/badge/license-Unlicense-blue.svg",
+  },
+  WTFPL: {
+    url: "http://www.wtfpl.net/about/",
+    imageUrl: "https://img.shields.io/badge/License-WTFPL-brightgreen.svg",
+  },
+  Zlib: {
+    url: "https://opensource.org/licenses/Zlib",
+    imageUrl: "https://img.shields.io/badge/License-Zlib-lightgrey.svg",
+  },
+  //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+};
+
+let chosenLicence = licences["GNU GPL v3"];
+console.log(chosenLicence.url);
+console.log(chosenLicence.imageUrl);
 
 const promptUser = () =>
   inquirer.prompt([
@@ -51,7 +218,6 @@ const promptUser = () =>
         "Boost Software License 1.0",
         "BSD 3-Clause License",
         "BSD 2-Clause License",
-        "Creative Commons***need to filter further***",
         "CC0",
         "CC0 1.0",
         "Attribution 4.0 International",
@@ -114,35 +280,46 @@ const promptUser = () =>
       name: "linkedin",
       message: "Enter your LinkedIn URL.",
     },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address",
+    },
   ]);
+
+// const { dirname } = require("path");
+// const appDir = dirname(require.main.filename);
 
 const generateREADME = (answers) =>
   `![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
   ![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)(https://opensource.org/licenses/BSD-2-Clause)
+  Shields.io has autogenerated license badges: 
+  https://img.shields.io/github/license/<Github-Username>/<Repository>
+  
   # Title
 
   
   ## Table of Contents
-  [Description](#description)
-  [Installation](#installation)
-  [Usage](#usage)
-  [License](#license)
-  [Contributing](#contributing)
-  [Tests](#tests)
+  [Description](#description)<br>
+  [Installation](#installation)<br>
+  [Usage](#usage)<br>
+  [License](#license)<br>
+  [Contributing](#contributing)<br>
+  [Tests](#tests)<br>
   [Questions](#questions)
 
   ## Description
   ${answers.description}
   ## Installation
-  These steps should be followed to install this project: ${answers.installation}.
+  These steps should be followed to install this project: ${answers.installation}
   ## Usage
-  This project should be used to ${answers.usage}.
+  This project should be used to ${answers.usage}
   ## License
   This project holds a ${answers.license} license.
   ## Contributing
-  The following: ${answers.contributing} contributed to this project.
+  The following contributed to this project: ${answers.contributing}
   ## Tests
-  The project should be test in the following way: ${answers.testing}.
+  ${answers.testing}
   ## Questions
   
   My GitHub username is ${answers.github}.
